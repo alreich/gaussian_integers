948349a083c54f0d95d0971edc26cf3285da02c9
Al Reich <al.reich@gmail.com>
2024-03-29 12:12:09 -0500

    WIP Quat Ints

--- src/quaternions.py
+++ src/quaternions.py
@@ -13,25 +13,49 @@ from gaussians import Zi
 
 
 class Hi:
-    """Integer-valued quaternions ('Gaussian quaternions')"""
+    """Integer-valued quaternions ('Gaussian quaternions')
 
+    Internally, a quaternion integer is implemented as a numpy array of 4
+    integers, but they can be thought of as simply 4 integers, or 2 Gaussian
+    integers. For this reason, a Hi can be constructed from 4 integers, or
+    2 Gaussian integers, or 1 numpy array of 4 integers.
+    """
     def __init__(self,
-                 a: (int, np.int64) = 1,
-                 b: (int, np.int64) = 0,
-                 c: (int, np.int64) = 0,
-                 d: (int, np.int64) = 0):
-        if isinstance(a, (int, np.int64)):
-            self.__arr = np.array([a, b, c, d], dtype=np.int64)
-        elif isinstance(a, np.ndarray):
-            self.__arr = a
+                 # If no inputs are given, then the "zero" quaternion will be returned
+                 z1: (Zi, np.ndarray, int) = Zi(),
+                 z2: (Zi, int) = Zi(),
+                 z3: int = 0,
+                 z4: int = 0):
+
+        # Assumes two Gaussian integers provided, z1 & z2
+        if isinstance(z1, Zi):
+            self.__arr = np.array([z1.real, z1.imag, z2.real, z2.imag], dtype=np.int64)
+
+        # Assumes one numpy array containing four np.int64's provided, z1
+        elif isinstance(z1, np.ndarray):
+            self.__arr = z1
+
+        # Assumes four ints or np.int64s provided, z1, ..., z4
+        elif isinstance(z1, (int, np.int64)):
+            self.__arr = np.array([z1, z2, z3, z4], dtype=np.int64)
+        else:
+            raise ValueError(f"{type(z1)} not supported")
 
     @property
-    def real(self) -> int:
-        return int(self.__arr[0])
+    def gaussian_ints(self):
+        arr = self.__arr
+        n = len(arr) // 2
+        return Zi.from_array(arr[:n]), Zi.from_array(arr[n:])
+
+    @property
+    def real(self):
+        real, _ = self.gaussian_ints
+        return real
 
     @property
     def imag(self):
-        return tuple(self.__arr[1:])
+        _, imag = self.gaussian_ints
+        return imag
 
     @property
     def array(self):
@@ -39,13 +63,17 @@ class Hi:
 
     @property
     def conjugate(self):
-        a, b, c, d = self.array
-        return Hi(a, -b, -c, -d)
+        real, imag = self.gaussian_ints
+        return Hi(real.conjugate, -imag)
 
     @property
     def norm(self) -> int:
         tmp = self * self.conjugate
-        return int(tmp.real)
+        return int(tmp.array[0])
+
+    def show(self):
+        real, imag = self.gaussian_ints
+        return f"Hi({repr(real)}, {repr(imag)})"
 
     def __abs__(self) -> float:
         return math.sqrt(self.norm)
@@ -68,14 +96,25 @@ class Hi:
         return Hi(-self.array)
 
     def __eq__(self, other):
-        return self.array == other.array
+        return np.array_equal(self.array, other.array)
 
     def __ne__(self, other):
-        return self.array != other.array
+        return not self == other
 
     def __mul__(self, other):
-        a1, b1, c1, d1 = self.array
-        a2, b2, c2, d2 = other.array
+        """Multiplication according to the Cayley-Dickson construction"""
+        a, b = self.gaussian_ints
+        c, d = other.gaussian_ints
+        # (a, b) * (c, d) = (a * c - d.conj * b, d * a + b * c.conj)
+        z1 = a * c - d.conjugate * b
+        z2 = d * a + b * c.conjugate
+        return Hi(z1, z2)
+
+    @staticmethod
+    def hamilton_product(q1, q2):
+        """Multiplication according to the classic Hamilton product"""
+        a1, b1, c1, d1 = q1.array
+        a2, b2, c2, d2 = q2.array
         # See https://en.wikipedia.org/wiki/Quaternion#Hamilton_product
         a = a1 * a2 - b1 * b2 - c1 * c2 - d1 * d2
         b = a1 * b2 + b1 * a2 + c1 * d2 - d1 * c2
@@ -103,19 +142,6 @@ class Hi:
         return Zi(int(a), int(b)), Zi(int(c), int(d))
 
     @staticmethod
-    def mul_as_gaussian_ints(q1, q2):
-        """Implements quaternion multiplication by first converting each one into
-        two Gaussian integers, Zi, and then multiplying the Zi according to the
-        Cayley-Dickson construction
-        """
-        a, b = q1.to_gaussian_ints()
-        c, d = q2.to_gaussian_ints()
-        # (a, b) * (c, d) = (a * c - d.conj * b, d * a + b * c.conj)
-        z1 = a * c - d.conjugate * b
-        z2 = d * a + b * c.conjugate
-        return Hi(z1.real, z1.imag, z2.real, z2.imag)
-
-    @staticmethod
     def random(low=-100, high=100):
         return Hi(np.array([rnd.randint(low, high) for _ in range(4)]))
 
@@ -128,7 +154,13 @@ class Hi:
         r = a - b * q
         return q, r
 
+    @staticmethod
+    def from_string(s):
+        """Converts the string form of a Hi back into a Hi.
 
-def split_array(arr):
-    n = len(arr) // 2
-    return arr[:n], arr[n:]
+        Example:
+        Hi.from_string('(46 + -92i + 9j + 23k)') -> Hi(46, -92, 9, 23)
+        """
+        return Hi(np.array(list(map(lambda x: int(x),
+                                    s.translate({ord(i): None for i in 'ijk'}
+                                                )[1:-1].split(' + ')))))
