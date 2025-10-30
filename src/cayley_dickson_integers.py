f93cfa2ccfae7a1d4fcfc012eadaa428892939fe
Alfred Reich <al.reich@gmail.com>
2025-10-30 15:15:59 -0500

    fubar

--- src/cayley_dickson_integers.py
+++ src/cayley_dickson_integers.py
@@ -426,7 +426,7 @@ class Zi(CayleyDicksonBase):
             im = Zi(quat[2], quat[3])
             return Zi(re, im)
         elif isinstance(quat, str):
-            return Zi(Zi.parse_quaternion_string(quat))
+            return Zi(Zi.parse_hypercomplex_string(quat))
         else:
             raise ValueError(f"Cannot create a quaternion from {quat}")
 
@@ -451,7 +451,7 @@ class Zi(CayleyDicksonBase):
             raise Exception(f"{self} is not a quaternion")
 
     def octonion_to_string(self):
-        unit_strs = ["", "i", "j", "k", "L", "Li", "Lj", "Lk"]
+        unit_strs = ["", "i", "j", "k", "L", "I", "J", "K"]
         if self.is_octonion():
             qstr = ""
             for idx, coef in enumerate(list(utils.flatten(self.to_array()))):
@@ -569,113 +569,165 @@ class Zi(CayleyDicksonBase):
     #     else:
     #         return False
 
+    # @staticmethod
+    # def parse_quaternion_string(qstr):
+    #     """Parse a complex/quaternion/octonion string into an array that
+    #     can be used to instantiate a Zi. The string can be formatted in
+    #     many different ways.
+    #     * It may be normal: 1+2i, or 1+2i-3j-4k, or 1+2i-3j-4k-5L+6Li-7Lj+8Lk
+    #     * It may have missing terms: 1-3k, 2i-4k, 2j-8Lk
+    #     * It may have missing coefficients: i+j-k, j+L (they are assumed to be 1)
+    #     * It may not be in the right order: 2i-1+3k-4j, L-k
+    #     * The coefficients must be ints, floats, or scientific notation: 7-2.4e-3i+3.75j-4.0k
+    #     * There will always be a single + or - between terms
+    #     * All inputs must be valid with at least 1 term, and without repeated units (2j - 3j)
+    #     """
+    #
+    #     def make_term(tm):
+    #         """Return a pair where the first element is one of 'real', 'i', 'j', 'k',
+    #         'L', 'Li', 'Lj', or 'Lk', and the second element is the coefficient as a
+    #         float or int. These will be used to update a dictionary."""
+    #
+    #         # Pattern for a valid quaternion term that ends in i, j, k, L, Li, Lj, or Lk
+    #         unit_term_pat = r'^[-+]?((\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)?([lL]|[lL][ijk]|[ijk])$'
+    #
+    #         # The term is either associated with a unit (i,j,k) or it's 'real'
+    #         if re.match(unit_term_pat, tm):
+    #             if 'li' in tm or 'lj' in tm or 'lk' in tm or 'Li' in tm or 'Lj' in tm or 'Lk' in tm:
+    #                 m = -2
+    #             else:
+    #                 m = -1
+    #             return tm[m:], utils.make_int_or_float(tm[:m])  # e.g., ('i', 2.3), ('lk', 7)
+    #         else:
+    #             return 'real', utils.make_int_or_float(tm)  # e.g., ('real', -3.1)
+    #
+    #     def maybe_add_coefficient(tm):
+    #         """If a term consists of a single unit (i, j, k), then put
+    #         a coefficient of 1 on it, otherwise just return the term."""
+    #
+    #         unit_strings = ['i', 'j', 'k', 'l', 'li', 'lj', 'lk', 'L', 'Li', 'Lj', 'Lk']
+    #         if tm in unit_strings:
+    #             return '1' + tm
+    #         else:
+    #             return tm
+    #
+    #     # The strategy below is to perform a succession of simple edits on
+    #     # the string to turn it into a 4-element array, rather than to try
+    #     # to write some enormous, unreadable regular expression. The regex's
+    #     # used here are already a bit difficult to read.
+    #
+    #     # Make lowercase and remove all spaces
+    #     q0 = qstr.lower().strip().replace(' ', '')
+    #
+    #     # Put a coefficient of 1 in front of units where it is implied to be 1.
+    #     q00 = q0.replace('+i', '+1i').replace('+j', '+1j').replace('+k', '+1k')
+    #     q01 = q00.replace('-i', '-1i').replace('-j', '-1j').replace('-k', '-1k')
+    #     q02 = q01.replace('+Li', '+1Li').replace('+Lj', '+1Lj').replace('+Lk', '+1Lk')
+    #     q03 = q02.replace('-Li', '-1Li').replace('-Lj', '-1Lj').replace('-Lk', '-1Lk')
+    #     q04 = q03.replace('+li', '+1li').replace('+lj', '+1lj').replace('+lk', '+1lk')
+    #     q05 = q04.replace('-li', '-1li').replace('-lj', '-1lj').replace('-lk', '-1lk')
+    #     q06 = q05.replace('-L-', '-1L-').replace('-L+', '-1L+').replace('+L-', '+1L-').replace('+L+', '+1L+')
+    #     q0b = q06.replace('-l-', '-1l-').replace('-l+', '-1l+').replace('+l-', '+1l-').replace('+l+', '+1l+')
+    #
+    #     # Put single space in front of + & -
+    #     q1 = q0b.replace('+', ' +').replace('-', ' -')
+    #
+    #     # If scientific notation, remove the space that was added in the previous step
+    #     q1a = q1.replace('e -', 'e-').replace('e +', 'e+')
+    #
+    #     # Remove any space after leading parenthesis (created by the step above)
+    #     q2 = q1a.replace('( ', '(')
+    #
+    #     # Remove parentheses, if they exist
+    #     q3 = q2.replace('(', '').replace(')', '').strip()
+    #
+    #     # Split string at spaces.
+    #     # The input string will now be transformed into a list of strings that
+    #     # correspond to terms in the quaternion.
+    #     q4 = q3.split()
+    #
+    #     # Some terms are just units (i, j, k), possibly with a sign (-+)
+    #     # Add a coefficient of 1 or -1 to those terms.
+    #     q5 = [maybe_add_coefficient(t) for t in q4]
+    #
+    #     # Make sure each individual term in the string is a valid term.
+    #     # That is, a possible sign (+-), possibly followed by a number,
+    #     # possibly followed by a unit element (i, j, k, l, li, lj, lk).
+    #     # NOTE: The only difference between the re pattern, below, and the
+    #     #       one used earlier, above, is the '?' near the very end.
+    #     qterm_pat = r'^[-+]?((\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)?([lL]|[lL][ijk]|[ijk])?$'
+    #     for term in q5:
+    #         mat = re.match(qterm_pat, term)
+    #         if mat is None:
+    #             raise ValueError(f"{term} in {qstr} is not a valid quaternion term.")
+    #
+    #     # Each call to make_term returns a key-value pair that can be used to
+    #     # update the dictionary, qdict, where the key is one of 'real', 'i',
+    #     # 'j', 'k', 'l', 'li', 'lj', or 'lk', and the value is the coefficient
+    #     # (float or int) that corresponds to the key.
+    #     q6 = [make_term(t) for t in q5]
+    #     qdict = {'real': 0, 'i': 0, 'j': 0, 'k': 0, 'l':0, 'li':0, 'lj':0, 'lk':0}
+    #     for term in q6:
+    #         qdict[term[0]] = term[1]
+    #     result = list(qdict.values())
+    #
+    #     # If the last 4 elements of the result are all zero, then we've parsed
+    #     # a quaternion string, so just return the first 4 elements, but if the
+    #     # last half of those 4 are also zero, then we've parsed a complex number,
+    #     # so, in that case, just return the first 2 elements. Otherwise, we've
+    #     # parsed an octonion, so return the entire 8-element list of coefficients.
+    #     if all(x == 0 for x in result[4:]):
+    #         if all(x == 0 for x in result[2:4]):
+    #             return result[:2]  # complex (2 elements)
+    #         else:
+    #             return result[:4]  # quaternion (4 elements)
+    #     else:
+    #         return result  # octonion (8 elements)
+
     @staticmethod
-    def parse_quaternion_string(qstr):
-        """Parse a complex/quaternion/octonion string into an array that
-        can be used to instantiate a Zi. The string can be formatted in
-        many different ways.
-        * It may be normal: 1+2i, or 1+2i-3j-4k, or 1+2i-3j-4k-5L+6Li-7Lj+8Lk
-        * It may have missing terms: 1-3k, 2i-4k, 2j-8Lk
-        * It may have missing coefficients: i+j-k, j+L (they are assumed to be 1)
-        * It may not be in the right order: 2i-1+3k-4j, L-k
-        * The coefficients must be ints, floats, or scientific notation: 7-2.4e-3i+3.75j-4.0k
-        * There will always be a single + or - between terms
-        * All inputs must be valid with at least 1 term, and without repeated units (2j - 3j)
-        """
+    def parse_hypercomplex_string(qs):
 
-        def make_term(tm):
-            """Return a pair where the first element is one of 'real', 'i', 'j', 'k',
-            'L', 'Li', 'Lj', or 'Lk', and the second element is the coefficient as a
-            float or int. These will be used to update a dictionary."""
+        pat1 = r'[-+]?((\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)?[ijkLIJK]'
 
-            # Pattern for a valid quaternion term that ends in i, j, k, L, Li, Lj, or Lk
-            unit_term_pat = r'^[-+]?((\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)?([lL]|[lL][ijk]|[ijk])$'
+        # Remove spaces
+        qstr = qs.strip().replace(' ', '')
 
-            # The term is either associated with a unit (i,j,k) or it's 'real'
-            if re.match(unit_term_pat, tm):
-                if 'li' in tm or 'lj' in tm or 'lk' in tm or 'Li' in tm or 'Lj' in tm or 'Lk' in tm:
-                    m = -2
-                else:
-                    m = -1
-                return tm[m:], utils.make_int_or_float(tm[:m])  # e.g., ('i', 2.3), ('lk', 7)
+        # This constructs the of terms (as strings) involving i, j, and k,
+        # but it does not include the real term, if there is one.
+        terms = []
+        real = None
+        for match in re.finditer(pat1, qstr):
+            trm = match.group(0)
+            terms.append(trm)
+
+        # Use the process of elimination to find the real term.
+        for tm in terms:
+            qstr = qstr.replace(tm, '')
+        if qstr == '':
+            real = '0'
+        else:
+            real = qstr  # Whatever is left over is the real coefficient (string)
+
+        # Initialize a dictionary of term coefficients.
+        term_dict = {'real': utils.make_int_or_float(real), 'i':0, 'j': 0, 'k': 0, 'L':0, 'I':0, 'J':0, 'K':0}
+
+        # Convert coefficient strings into numbers;
+        # also, make implied coefficients of 1 or -1 explicit.
+        for tm in terms:
+            coeff_str = tm[:-1]
+            if coeff_str == '+' or coeff_str == '':
+                coeff_str = '1'
+            elif coeff_str == '-':
+                coeff_str = '-1'
             else:
-                return 'real', utils.make_int_or_float(tm)  # e.g., ('real', -3.1)
+                pass
+            unit_str = tm[-1:]
+            term_dict[unit_str] = utils.make_int_or_float(coeff_str)
 
-        def maybe_add_coefficient(tm):
-            """If a term consists of a single unit (i, j, k), then put
-            a coefficient of 1 on it, otherwise just return the term."""
-
-            unit_strings = ['i', 'j', 'k', 'l', 'li', 'lj', 'lk', 'L', 'Li', 'Lj', 'Lk']
-            if tm in unit_strings:
-                return '1' + tm
-            else:
-                return tm
-
-        # The strategy below is to perform a succession of simple edits on
-        # the string to turn it into a 4-element array, rather than to try
-        # to write some enormous, unreadable regular expression. The regex's
-        # used here are already a bit difficult to read.
-
-        # Make lowercase and remove all spaces
-        q0 = qstr.lower().strip().replace(' ', '')
-
-        # Put a coefficient of 1 in front of units where it is implied to be 1.
-        q00 = q0.replace('+i', '+1i').replace('+j', '+1j').replace('+k', '+1k')
-        q01 = q00.replace('-i', '-1i').replace('-j', '-1j').replace('-k', '-1k')
-        q02 = q01.replace('+Li', '+1Li').replace('+Lj', '+1Lj').replace('+Lk', '+1Lk')
-        q03 = q02.replace('-Li', '-1Li').replace('-Lj', '-1Lj').replace('-Lk', '-1Lk')
-        q04 = q03.replace('+li', '+1li').replace('+lj', '+1lj').replace('+lk', '+1lk')
-        q05 = q04.replace('-li', '-1li').replace('-lj', '-1lj').replace('-lk', '-1lk')
-        q06 = q05.replace('-L-', '-1L-').replace('-L+', '-1L+').replace('+L-', '+1L-').replace('+L+', '+1L+')
-        q0b = q06.replace('-l-', '-1l-').replace('-l+', '-1l+').replace('+l-', '+1l-').replace('+l+', '+1l+')
-
-        # Put single space in front of + & -
-        q1 = q0b.replace('+', ' +').replace('-', ' -')
-
-        # If scientific notation, remove the space that was added in the previous step
-        q1a = q1.replace('e -', 'e-').replace('e +', 'e+')
-
-        # Remove any space after leading parenthesis (created by the step above)
-        q2 = q1a.replace('( ', '(')
-
-        # Remove parentheses, if they exist
-        q3 = q2.replace('(', '').replace(')', '').strip()
-
-        # Split string at spaces.
-        # The input string will now be transformed into a list of strings that
-        # correspond to terms in the quaternion.
-        q4 = q3.split()
-
-        # Some terms are just units (i, j, k), possibly with a sign (-+)
-        # Add a coefficient of 1 or -1 to those terms.
-        q5 = [maybe_add_coefficient(t) for t in q4]
-
-        # Make sure each individual term in the string is a valid term.
-        # That is, a possible sign (+-), possibly followed by a number,
-        # possibly followed by a unit element (i, j, k, l, li, lj, lk).
-        # NOTE: The only difference between the re pattern, below, and the
-        #       one used earlier, above, is the '?' near the very end.
-        qterm_pat = r'^[-+]?((\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)?([lL]|[lL][ijk]|[ijk])?$'
-        for term in q5:
-            mat = re.match(qterm_pat, term)
-            if mat is None:
-                raise ValueError(f"{term} in {qstr} is not a valid quaternion term.")
-
-        # Each call to make_term returns a key-value pair that can be used to
-        # update the dictionary, qdict, where the key is one of 'real', 'i',
-        # 'j', 'k', 'l', 'li', 'lj', or 'lk', and the value is the coefficient
-        # (float or int) that corresponds to the key.
-        q6 = [make_term(t) for t in q5]
-        qdict = {'real': 0, 'i': 0, 'j': 0, 'k': 0, 'l':0, 'li':0, 'lj':0, 'lk':0}
-        for term in q6:
-            qdict[term[0]] = term[1]
-        result = list(qdict.values())
-
-        # If the last 4 elements of the result are all zero, then we've parsed
-        # a quaternion string, so just return the first 4 elements, but if the
-        # last half of those 4 are also zero, then we've parsed a complex number,
-        # so, in that case, just return the first 2 elements. Otherwise, we've
-        # parsed an octonion, so return the entire 8-element list of coefficients.
+        # Convert the term dictionary into an ordered list of coefficients
+        # The length of the list returned depends on what type of string
+        # was input (complex, quaternion, octonion, etc.)
+        result = list(term_dict.values())
         if all(x == 0 for x in result[4:]):
             if all(x == 0 for x in result[2:4]):
                 return result[:2]  # complex (2 elements)
